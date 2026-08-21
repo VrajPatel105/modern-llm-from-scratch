81a4b73bf91dc8d43cb8d051ca136f98c5766831
VrajPatel105 <vrajp1052005@gmail.com>
2026-08-20 21:28:01 -0400

    feat(layers) : adding swiglu

--- model/layers.py
+++ model/layers.py
@@ -11,3 +11,16 @@ class RMSNorm(nn.Module):
     def forward(self,x):
         rms = (x*x).mean(dim=-1, keepdim=True) + self.eps
         return (x / torch.sqrt(rms)) * self.alpha
+
+
+class SwiGLU(nn.Module):
+    def __init__(self, d_model, d_ff_glu=None):
+        super().__init__()
+        d_ff_glu = d_ff_glu or int((8/3) * d_model)
+        self.w_in = nn.Linear(d_model, 2 * d_ff_glu)
+        self.w_out = nn.Linear(d_model, d_ff_glu, d_model)
+
+    def forward(self, x):
+        a,b = self.w_in(x).chunk(2, dim=-1)
+        gated = nn.SiLU(x) * b
+        return self.w_out(gated)
