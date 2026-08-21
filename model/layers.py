3610cb84e9d4e9eb38e3fed77e283f4bd244162a
VrajPatel105 <vrajp1052005@gmail.com>
2026-08-21 15:16:05 -0400

    feat(layers) : Adding finalized version of SwiGLU

--- model/layers.py
+++ model/layers.py
@@ -17,10 +17,11 @@ class SwiGLU(nn.Module):
     def __init__(self, d_model, d_ff_glu=None):
         super().__init__()
         d_ff_glu = d_ff_glu or int((8/3) * d_model)
+        self.silu = nn.SiLU()
         self.w_in = nn.Linear(d_model, 2 * d_ff_glu)
-        self.w_out = nn.Linear(d_model, d_ff_glu, d_model)
+        self.w_out = nn.Linear(d_ff_glu, d_model)
 
     def forward(self, x):
-        a,b = self.w_in(x).chunk(2, dim=-1)
-        gated = nn.SiLU(x) * b
+        a, b = self.w_in(x).chunk(2, dim=-1)
+        gated = self.silu(a) * b
         return self.w_out(gated)
